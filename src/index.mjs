db0335e8121375c0cda5fad709f2c350794efa85
quan <quan_xiaoxiao@qq.com>
2024-07-30 08:56:31 +0800

    xxx

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ import getPathname from './getPathname.mjs';
 import wrapStreamWrite from './wrapStreamWrite.mjs';
 import wrapStreamRead from './wrapStreamRead.mjs';
 import readFileList from './readFileList.mjs';
+import toDataify from './toDataify.mjs';
 
 export const sha256 = (str) => createHash('sha256')
   .update(str)
@@ -14,4 +15,5 @@ export {
   getPathname,
   wrapStreamWrite,
   wrapStreamRead,
+  toDataify,
 };
